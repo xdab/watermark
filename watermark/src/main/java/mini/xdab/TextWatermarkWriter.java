3e59b630f8c093ecd84a855484487d1066d4f91f
Przemysław Wcisło <01142222@pw.edu.pl>
2021-01-07 23:51:55 +0100

    + TextWatermarkWriter, defaults writing bytes as string repr

---
+++ watermark/src/main/java/mini/xdab/TextWatermarkWriter.java
@@ -0,0 +1,34 @@
+package mini.xdab;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+
+public class TextWatermarkWriter implements IWatermarkWriter {
+
+    protected Font font;
+    protected Color color;
+    protected double alpha;
+
+
+    @Override
+    public void write(BufferedImage img, byte[] data) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : data) {
+            if (font.canDisplay(b)) {
+                sb.append((char)b);
+            } else {
+                sb.append("\\x");
+                sb.append(Integer.toHexString(b));
+            }
+        }
+        write(img, sb.toString());
+        return;
+    }
+
+    @Override
+    public void write(BufferedImage img, String string) {
+        //TODO
+    }
+
+}
