6e19db507cf8606a14419721c11aed4f4d943dcb
Przemysław Wcisło <01142222@pw.edu.pl>
2021-01-08 00:43:15 +0100

    + BitUtils (core LSB func)

---
+++ watermark/src/main/java/mini/xdab/BitUtils.java
@@ -0,0 +1,14 @@
+package mini.xdab;
+
+public class BitUtils {
+
+    public static int getLSB(int of) {
+        return (of & 1);
+    }
+
+    public static int setLSB(int of, int toValue) {
+        //          Clear             Set
+        return (of ^ (of & 1)) | (toValue & 1);
+    }
+
+}
