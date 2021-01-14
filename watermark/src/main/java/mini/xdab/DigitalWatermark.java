f67182ea5483dabebee8eb6e8d3279beddf2944f
Przemyslaw Wcislo <przemyslawxdab@gmail.com>
2021-01-15 00:04:36 +0100

    defaulted readString on a higher level

--- watermark/src/main/java/mini/xdab/DigitalWatermark.java
+++ watermark/src/main/java/mini/xdab/DigitalWatermark.java
@@ -1,6 +1,7 @@
 package mini.xdab;
 
 import lombok.NonNull;
+import org.w3c.dom.Text;
 
 import java.awt.image.BufferedImage;
 import java.nio.charset.StandardCharsets;
@@ -11,6 +12,11 @@ public abstract class DigitalWatermark implements IWatermarkWriter, IWatermarkRe
     @Override
     public void write(@NonNull BufferedImage img, @NonNull String string) {
         write(img, string.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public String readString(@NonNull BufferedImage img) {
+        return TextUtils.bytesToString(read(img));
     }
 
 }
