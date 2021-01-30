be6d32b95b4f9b250d63b0e17a97e6e08c2a0461
Przemyslaw Wcislo <przemyslawxdab@gmail.com>
2021-01-30 19:28:45 +0100

    renamed option (r)epeat to (n)umber, making way for (r)ead flag

--- watermark/src/main/java/mini/xdab/OptionComparator.java
+++ watermark/src/main/java/mini/xdab/OptionComparator.java
@@ -1,5 +1,6 @@
 package mini.xdab;
 
+import mini.xdab.constants.OptionConstants;
 import org.apache.commons.cli.Option;
 import org.jetbrains.annotations.NotNull;
 
@@ -7,7 +8,21 @@ import java.util.Comparator;
 
 public class OptionComparator<T extends Option> implements Comparator<T> {
 
-    private static final String OPTS_ORDER = "iotmrlsbchvMV"; // short option names
+    private static final String OPTS_ORDER =
+            OptionConstants.FLAG_READ +
+            OptionConstants.FLAG_WRITE +
+            OptionConstants.SHORT_ARGUMENT_INPUT +
+            OptionConstants.SHORT_ARGUMENT_OUTPUT +
+            OptionConstants.SHORT_ARGUMENT_MESSAGE +
+            OptionConstants.SHORT_ARGUMENT_TYPE +
+            OptionConstants.SHORT_ARGUMENT_REPEAT +
+            OptionConstants.FLAG_HORIZONTAL +
+            OptionConstants.FLAG_VERTICAL +
+            OptionConstants.FLAG_MAJORITY +
+            OptionConstants.TYPE_ALIAS_LSB +
+            OptionConstants.TYPE_ALIAS_STRIPES +
+            OptionConstants.TYPE_ALIAS_BLOCKS +
+            OptionConstants.TYPE_ALIAS_CONSTELLATION;
 
     @Override
     public int compare(@NotNull T o1, @NotNull T o2) {
--- watermark/src/main/java/mini/xdab/constants/OptionConstants.java
+++ watermark/src/main/java/mini/xdab/constants/OptionConstants.java
@@ -6,13 +6,13 @@ public class OptionConstants {
     public static final String SHORT_ARGUMENT_OUTPUT = "o";
     public static final String SHORT_ARGUMENT_TYPE = "t";
     public static final String SHORT_ARGUMENT_MESSAGE = "m";
-    public static final String SHORT_ARGUMENT_REPEAT = "r";
+    public static final String SHORT_ARGUMENT_REPEAT = "n";
 
     public static final String LONG_ARGUMENT_INPUT = "input";
     public static final String LONG_ARGUMENT_OUTPUT = "output";
     public static final String LONG_ARGUMENT_TYPE = "type";
     public static final String LONG_ARGUMENT_MESSAGE = "message";
-    public static final String LONG_ARGUMENT_REPEAT = "repeat";
+    public static final String LONG_ARGUMENT_REPEAT = "number";
 
     public static final String TYPE_ALIAS_LSB = "lsb";
     public static final String TYPE_ALIAS_STRIPES = "stripes";
