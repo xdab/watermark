33bc9a4782cad8443842ac1ba16fdf6144738aa2
Przemyslaw Wcislo <przemyslawxdab@gmail.com>
2021-01-30 14:20:19 +0100

    moved main application back to Main

--- watermark/src/main/java/mini/xdab/Main.java
+++ watermark/src/main/java/mini/xdab/Main.java
@@ -1,5 +1,11 @@
 package mini.xdab;
 
+import mini.xdab.singleton.Log;
+import mini.xdab.singleton.Options;
+import mini.xdab.singleton.Strings;
+import mini.xdab.utils.ImageUtils;
+import mini.xdab.utils.OptionUtils;
+
 /**
  * Hello world!
  *
@@ -8,8 +14,26 @@ public class Main
 {
 
     public static void main(String[] args) {
-        var wmApp = new WatermarkApp();
-        wmApp.runFromArgs(args);
+        Options.setParsedArgs(OptionUtils.parseArgsToCmd(args));
+
+        run();
+    }
+
+
+    private static void run() {
+        var writer = Options.getWriter();
+        var reader = Options.getReader();
+
+        var img = ImageUtils.loadFromFile(Options.getInput());
+
+        writer.write(img, Options.getMessage());
+        ImageUtils.saveToFile(img, Options.getOutput());
+
+        if (reader != null) {
+            var img2 = ImageUtils.loadFromFile(Options.getOutput());
+            String readMessage = reader.readString(img2);
+            System.out.println("Decoded message: '" + readMessage + "'");
+        }
     }
 
 }
