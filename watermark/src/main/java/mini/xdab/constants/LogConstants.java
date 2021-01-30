17ec6d5e2c59cf3b8d4aa72ba2a24a715bda9727
Przemyslaw Wcislo <przemyslawxdab@gmail.com>
2021-01-30 14:17:56 +0100

    added independent Log(ger) utility

---
+++ watermark/src/main/java/mini/xdab/constants/LogConstants.java
@@ -0,0 +1,12 @@
+package mini.xdab.constants;
+
+public class LogConstants {
+
+    public static final Integer LEVEL_DEBUG =   0;
+    public static final Integer LEVEL_INFO  = 100;
+    public static final Integer LEVEL_WARN  = 200;
+    public static final Integer LEVEL_ERROR = 300;
+
+    public static final String PROPERTIES_FILE = "log";
+
+}
---
+++ watermark/src/main/java/mini/xdab/singleton/Log.java
@@ -0,0 +1,100 @@
+package mini.xdab.singleton;
+
+import lombok.Getter;
+import lombok.NonNull;
+import mini.xdab.constants.LogConstants;
+import mini.xdab.utils.TextUtils;
+import org.jetbrains.annotations.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class Log {
+
+    @Getter
+    private static Integer loggingLevel;
+
+    @Getter
+    private static boolean useStdErr;
+
+    private static boolean isInitialized = false;
+    private static SimpleDateFormat dateTimeFormat;
+
+
+    public static void debug(@NonNull String message, @Nullable Object... formatArgs) {
+        initialize(); log(String.format(message, formatArgs), LogConstants.LEVEL_DEBUG);
+    }
+
+    public static void info(@NonNull String message, @Nullable Object... formatArgs) {
+        initialize(); log(String.format(message, formatArgs), LogConstants.LEVEL_INFO);
+    }
+
+    public static void warn(@NonNull String message, @Nullable Object... formatArgs) {
+        initialize(); log(String.format(message, formatArgs), LogConstants.LEVEL_WARN);
+    }
+
+    public static void error(@NonNull String message, @Nullable Object... formatArgs) {
+        initialize(); log(String.format(message, formatArgs), LogConstants.LEVEL_ERROR);
+    }
+
+
+    private static void initialize() {
+        if (!isInitialized) {
+            var properties =  ResourceBundle.getBundle(LogConstants.PROPERTIES_FILE);
+
+            var loggingLevelStr = properties.getString("logging-level");
+            parseLoggingLevelString(loggingLevelStr);
+
+            var useStdErrStr = properties.getString("use-stderr");
+            parseUseStdErrString(useStdErrStr);
+
+            dateTimeFormat = new SimpleDateFormat(properties.getString("datetime-str-pattern"));
+            isInitialized = true;
+        }
+    }
+
+
+    private static void log(String message, Integer level) {
+        if (level < loggingLevel)
+            return;
+
+        String dateFormat = dateTimeFormat.format(Date.from(Instant.now()));
+        String formatted = String.format("LOG LV %03d @ %s > %s", level, dateFormat, message);
+
+        if (useStdErr)
+            System.err.println(formatted);
+        else
+            System.out.println(formatted);
+    }
+
+    private static void parseLoggingLevelString(String loggingLevelStr) {
+        loggingLevelStr = TextUtils.stripAndLower(loggingLevelStr);
+
+        switch (loggingLevelStr) {
+            case "error":
+                loggingLevel = LogConstants.LEVEL_ERROR;
+                break;
+
+            case "warn":
+                loggingLevel = LogConstants.LEVEL_WARN;
+                break;
+
+            case "info":
+                loggingLevel = LogConstants.LEVEL_INFO;
+                break;
+
+            default:
+            case "debug":
+                loggingLevel = LogConstants.LEVEL_DEBUG;
+                break;
+        }
+    }
+
+    private static void parseUseStdErrString(String useStdErrStr) {
+        useStdErrStr = TextUtils.stripAndLower(useStdErrStr);
+        useStdErr = Boolean.parseBoolean(useStdErrStr);
+    }
+
+}
