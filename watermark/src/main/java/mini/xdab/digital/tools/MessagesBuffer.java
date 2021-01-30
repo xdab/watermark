760d29396bbb1819fd0e2dc3e736c096cfc59417
Przemyslaw Wcislo <przemyslawxdab@gmail.com>
2021-01-31 00:23:45 +0100

    added a wrapper for processing probable messages buffer arrangement

---
+++ watermark/src/main/java/mini/xdab/digital/tools/MessagesBuffer.java
@@ -0,0 +1,49 @@
+package mini.xdab.digital.tools;
+
+import lombok.NonNull;
+import mini.xdab.singleton.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class MessagesBuffer {
+
+    private ByteArrayOutputStream mainBuffer;
+    private ByteArrayOutputStream tempBuffer;
+
+    public MessagesBuffer() {
+        mainBuffer = new ByteArrayOutputStream();
+        tempBuffer = new ByteArrayOutputStream();
+    }
+
+    public Integer size() {
+        return mainBuffer.size() + tempBuffer.size();
+    }
+
+    public void beginMessage() {
+        tempBuffer.reset();
+    }
+
+    public void addMessageByte(@NonNull Byte b) {
+        tempBuffer.write(b);
+    }
+
+    public void abortMessage() {
+        tempBuffer.reset();
+    }
+
+    public void concludeMessage() {
+        if (tempBuffer.size() > 0) {
+            try { mainBuffer.write(tempBuffer.toByteArray()); }
+            catch (IOException ioe) {
+                Log.error("MessagesBuffer.concludeMessage Unable to write from temp to main buffer: %s", ioe.getMessage());
+            }
+        }
+        tempBuffer.reset();
+    }
+
+    public byte[] getMessages() {
+        return mainBuffer.toByteArray();
+    }
+
+}
